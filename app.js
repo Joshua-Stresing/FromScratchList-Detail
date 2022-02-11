bf909154685152f080c702df2fb6a87b5228b46d
Joshua Stresing <pyrotorch69@yahoo.com>
2022-02-11 12:41:12 -0800

    worked out a bunch of my issues and remade some of the guts

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-// import functions and grab DOM elements
+import { consoles } from './data.js';
+import { renderListItem } from './render-utils.js';
 
-// let state
+const main = document.querySelector('main');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+for (let console of consoles) {
+    const consoleDiv = renderListItem(console);
+    main.append(consoleDiv);
+}
---
+++ game consoles/consoles.js
@@ -0,0 +1,1 @@
+import { consoles } from '../data.js';
